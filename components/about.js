fe7be942c767ac27d4b9b6bd9314f4a201765fc9
rodwin <rodwinchestermail@gmail.com>
2021-07-02 11:19:23 +0800

    Added header, hero animation, and About

--- components/about.js
+++ components/about.js
@@ -8,8 +8,8 @@ export default function About() {
             <Image src={'/images/resto-indoor.jpg'} width={700} height={500} />
             <div className={styles.text}>
                 <h1>About Us</h1>
-                <p>Temporibus nulla eius aliquam porro adipisci sunt. Totam eum et sint facere maiores. Laboriosam veritatis expedita et impedit ut iste quibusdam.</p>
-                <p>Consequatur sint et officia. Nisi aut quod et sed possimus ut a odio. Magni quaerat omnis vel odit necessitatibus dolor deleniti. Veniam perferendis veniam minima tempore ratione non sit sit. Magnam dolor eveniet ipsum eum unde et mollitia et.</p>
+                <p>Home of the finest classic burgers and steaks. Whether you come in for drinks before dinner or to enjoy a group of friends after a great game day, you will always find what you're looking for.</p>
+                <p>Celebrate the good days by savoring the great food at Rodwin's Tavern. Our attentive staff's demeanor and welcoming atmosphere is the perfect accompaniment to your next night on the town. And our burgers and steaks are flanked with an array of fresh vegetables and other premium offerings. So stop in today with your friends, or make a reservation for the whole gang.</p>
             </div>
         </div>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
+import Head from 'next/head';
+
 import Layout from '../components/layout/layout';
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Rodwin's Tavern</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
