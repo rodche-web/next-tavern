c55e336d4f15d58690d2bcde1d2cd9ad87fdc5db
rodwin <rodwinchestermail@gmail.com>
2021-07-02 11:49:00 +0800

    Fixed build bug

--- pages/menu/[productId].js
+++ pages/menu/[productId].js
@@ -4,7 +4,12 @@ import ItemDetail from '../../components/item-detail';
 
 export default function ProductDetail() {
     const router = useRouter();
-    const menuItem = menuItems.find(item => item.id === router.query.productId)
+    const menuItem = menuItems.find(item => item.id === router.query.productId);
+    if (!menuItem) {
+        return (
+            <h3>Loading</h3>
+        )
+    }
     return (
         <>
             <ItemDetail name={menuItem.name} price={menuItem.price} image={menuItem.image} description={menuItem.description} />
